c05dbc0b3e9afb825e2a667d0e16752e37ba2e9f
aimi <aimi@isi-grp.co.jp>
2021-09-17 17:36:08 +0900

    ;

--- api/common.js
+++ api/common.js
@@ -1,3 +1,3 @@
 exports.getStockById = async (db, id) => {
-  return await db.query(`SELECT id, name, price, on_sale, count FROM stock WHERE id = ${id}`);
+  return await db.query(`SELECT id, name, price, on_sale, count FROM stock WHERE id = ${id};`);
 }
